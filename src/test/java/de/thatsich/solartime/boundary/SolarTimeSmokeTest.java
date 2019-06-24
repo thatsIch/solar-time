7f92ce1a439cdc91bce53b975de4f68aad1a8a7c
thatsIch <thatsIch@mail.de>
2019-06-24 11:56:53 +0200

    Apparently manual editing of argLine affects the JaCoCo Agent

---
+++ src/test/java/de/thatsich/solartime/boundary/SolarTimeSmokeTest.java
@@ -0,0 +1,35 @@
+package de.thatsich.solartime.boundary;
+
+import de.thatsich.solartime.control.DateConverter;
+import de.thatsich.solartime.control.DawnCalculator;
+import de.thatsich.solartime.control.DuskCalculator;
+import de.thatsich.solartime.control.HourAngleCalculator;
+import de.thatsich.solartime.control.JulianSunriseCalculator;
+import de.thatsich.solartime.control.JulianSunsetCalculator;
+import de.thatsich.solartime.control.SolarEquationVariableCalculator;
+import de.thatsich.solartime.control.SolarNoonCalculator;
+import de.thatsich.solartime.control.TimeZoneShifter;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+
+class SolarTimeSmokeTest {
+
+    @Test
+    void setupSolarTime() {
+        final var dateConverter = new DateConverter();
+        final var solarEquationVariableCalculator = new SolarEquationVariableCalculator(dateConverter);
+        final var hourAngleCalculator = new HourAngleCalculator();
+        final var julianSunsetCalculator = new JulianSunsetCalculator(solarEquationVariableCalculator, hourAngleCalculator);
+        final var julianSunriseCalculator = new JulianSunriseCalculator(julianSunsetCalculator, solarEquationVariableCalculator);
+        final var timeZoneShifter = new TimeZoneShifter();
+        final var duskCalculator = new DuskCalculator(julianSunsetCalculator, dateConverter, timeZoneShifter);
+        final var dawnCalculator = new DawnCalculator(julianSunriseCalculator, dateConverter, timeZoneShifter);
+        final var solarNoonCalculator = new SolarNoonCalculator(solarEquationVariableCalculator, hourAngleCalculator, dateConverter);
+        final ThrowableAssert.ThrowingCallable solarTime = () -> new SolarTime(dawnCalculator, duskCalculator, solarNoonCalculator);
+
+        Assertions.assertThatCode(solarTime)
+                .doesNotThrowAnyException();
+    }
+
+}
