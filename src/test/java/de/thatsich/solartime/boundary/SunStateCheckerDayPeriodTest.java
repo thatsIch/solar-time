36fb18828ab3e732b27e14c15ba9c18d79968e6a
thatsIch <thatsIch@mail.de>
2019-06-25 11:23:10 +0200

    Added more tests for DayPeriod

--- src/test/java/de/thatsich/solartime/boundary/SunStateCheckerDayPeriodTest.java
+++ src/test/java/de/thatsich/solartime/boundary/SunStateCheckerDayPeriodTest.java
@@ -13,7 +13,7 @@ class SunStateCheckerDayPeriodTest {
     void isDay() {
         final var sunStateChecker = new API().getSunStateChecker();
 
-        final var day = ZonedDateTime.of(2019, 6, 24, 12, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var day = ZonedDateTime.of(2019, 1, 24, 12, 0, 0, 0, ZoneId.of("Europe/Berlin"));
         final var latitude = 51.449680;
         final var longitude = 6.973370;
 
@@ -21,6 +21,62 @@ class SunStateCheckerDayPeriodTest {
 
         Assertions.assertThat(actual)
                 .isEqualTo(DayPeriod.DAY);
+    }
+
+    @Test
+    void isCivilTwilight() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 1, 24, 8, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var latitude = 51.449680;
+        final var longitude = 6.973370;
+
+        final var actual = sunStateChecker.getDayPeriod(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isEqualTo(DayPeriod.CIVIL_TWILIGHT);
+    }
+
+    @Test
+    void isNauticalTwilight() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 1, 24, 7, 30, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var latitude = 51.449680;
+        final var longitude = 6.973370;
+
+        final var actual = sunStateChecker.getDayPeriod(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isEqualTo(DayPeriod.NAUTICAL_TWILIGHT);
+    }
+
+    @Test
+    void isAstronomicalTwilight() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 1, 24, 7, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var latitude = 51.449680;
+        final var longitude = 6.973370;
+
+        final var actual = sunStateChecker.getDayPeriod(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isEqualTo(DayPeriod.ASTRONOMICAL_TWILIGHT);
+    }
+
+    @Test
+    void isNight() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 1, 24, 6, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var latitude = 51.449680;
+        final var longitude = 6.973370;
+
+        final var actual = sunStateChecker.getDayPeriod(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isEqualTo(DayPeriod.NIGHT);
     }
 
 }
