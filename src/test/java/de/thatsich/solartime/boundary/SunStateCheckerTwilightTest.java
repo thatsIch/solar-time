bf88629a24184d4aa13a30b54497b0d942c4ffe0
thatsIch <thatsIch@mail.de>
2019-06-25 10:32:34 +0200

    Added twilight test

--- src/test/java/de/thatsich/solartime/boundary/SunStateCheckerTwilightTest.java
+++ src/test/java/de/thatsich/solartime/boundary/SunStateCheckerTwilightTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-class SunStateCheckerAstronomicalTwilightTest {
+class SunStateCheckerTwilightTest {
 
     @Test
-    @DisplayName("At 2019-01-24T6:30:00+02:00[Europe/Berlin] is Astronomical Twilight in Europe")
-    void isAstronomicalTwilight() {
+    @DisplayName("At 2019-01-24T6:30:00+02:00[Europe/Berlin] is Twilight in Europe")
+    void isTwilight() {
         final var sunStateChecker = new API().getSunStateChecker();
 
         final var day = ZonedDateTime.of(2019, 1, 24, 6, 30, 0, 0, ZoneId.of("Europe/Berlin"));
         final var latitude = 51.449680;
         final var longitude = 6.973370;
 
-        final var actual = sunStateChecker.isAstronomicalTwilight(day, latitude, longitude);
+        final var actual = sunStateChecker.isTwilight(day, latitude, longitude);
 
         Assertions.assertThat(actual)
                 .isTrue();
