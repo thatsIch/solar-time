73ecca9642d2e651800ec225f49255e2355f8b23
thatsIch <thatsIch@mail.de>
2019-06-25 10:06:35 +0200

    Added more tests for isDay

--- src/test/java/de/thatsich/solartime/boundary/SunStateCheckerIsDayTest.java
+++ src/test/java/de/thatsich/solartime/boundary/SunStateCheckerIsDayTest.java
@@ -1,6 +1,7 @@
 package de.thatsich.solartime.boundary;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZoneId;
@@ -20,6 +21,35 @@ class SunStateCheckerIsDayTest {
 
         Assertions.assertThat(actual)
                 .isTrue();
+    }
+
+    @Test
+    void isNotDay() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 6, 24, 1, 0, 0, 0, ZoneId.of("Europe/Berlin"));
+        final var latitude = 51.449680;
+        final var longitude = 6.973370;
+
+        final var actual = sunStateChecker.isDay(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isFalse();
+    }
+
+    @Test
+    @DisplayName("In Tromsø, Norway there is a 24h Night")
+    void alwaysNightAtPoles() {
+        final var sunStateChecker = new API().getSunStateChecker();
+
+        final var day = ZonedDateTime.of(2019, 12, 24, 12, 0, 0, 0, ZoneId.of("Europe/Istanbul"));
+        final var latitude = 69.660716;
+        final var longitude = 18.925278;
+
+        final var actual = sunStateChecker.isDay(day, latitude, longitude);
+
+        Assertions.assertThat(actual)
+                .isFalse();
     }
 
 }
